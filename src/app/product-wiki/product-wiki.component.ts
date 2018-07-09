0d995b13e54470a2675eb8c1317374ec0a48bff2
Giorgi <george@developerslab.net>
2018-07-09 15:25:31 +0400

    profile changed vol. 2 hope final

---
+++ src/app/product-wiki/product-wiki.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-wiki',
+  templateUrl: './product-wiki.component.html',
+  styleUrls: ['./product-wiki.component.css']
+})
+export class ProductWikiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
